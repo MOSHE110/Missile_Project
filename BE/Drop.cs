d81fecb49bfd14364a6e462f7e02a61ff49403ae
mosheco110 <mosheco110@gmail.com>
2018-12-18 15:56:57 +0200

    bf

--- BE/Drop.cs
+++ BE/Drop.cs
@@ -8,7 +8,9 @@ namespace BE
 {
     class Drop
     {
-        private string Drop_Adress { get; set; }
+        private string Real_Drop_Adress { get; set; }
+        private string Estimated_Drop_Adress { get; set; }
+        private double Mistake_Distance { get; set; }
         private DateTime Drop_time { get; set; }
         // private  string Drop_name { get; set; }
         private struct Lat_Long {public double Latitude;  public double Longitude;}  
